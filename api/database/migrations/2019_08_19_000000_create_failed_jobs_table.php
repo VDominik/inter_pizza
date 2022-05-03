f0583922fa3b88bee45b1bca0a3f4f848c52f8e9
GreenRockLee <44070959+GreenRockLee@users.noreply.github.com>
2022-05-03 15:36:04 +0200

    Revert "Feature/adding interactive class to components"

--- api/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ api/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('menu', function (Blueprint $table) {
+        Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('nazov');
-            $table->string('ingrediencie');
-            $table->float('cena');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
         });
     }
 
@@ -30,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('menu');
+        Schema::dropIfExists('failed_jobs');
     }
 };
