5fd553638868b8efc7a9075788217ec6b79396f3
Dominik <V.dominik11@gmail.com>
2022-05-16 17:11:37 +0200

    vytvorena galeria a pridany form do kosika

--- front/pages/kosik/index.js
+++ front/pages/kosik/index.js
@@ -1,15 +1,37 @@
 import Navbar from "../../comps/Navbar";
 import Footer from "../../comps/Footer";
+import styles from "../../styles/Kosik.module.css"
+import React from "react";
 
 export default function kosik() {
     return (
-        <div>
+        <div className={styles.container}>
 
             <Navbar />
 
+            <div className={styles.formwrapper}>
 
+                <form className={styles.form}>
+                        <label>Meno:</label><br/>
+                        <input className={styles.input} type="text"></input><br/>
 
-            <Footer />
+                        <label>Priezvisko:</label><br/>
+                        <input className={styles.input} type="text"></input><br/>
+
+                        <label>Ulica:</label><br/>
+                        <input className={styles.input} type="text"></input><br/>
+
+                        <label>Mesto:</label><br/>
+                        <input className={styles.input} type="text"></input><br/>
+
+                        <label>Tel. číslo:</label><br/>
+                        <input className={styles.input} type="text"></input><br/>
+
+                        <button className={styles.button} type={"button"}>Vložiť do pece</button>
+
+                </form>
+
+            </div>
 
         </div>
     )
